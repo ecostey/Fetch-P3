3b5394c545ff885fa52a10d37ebd61346d62c7cf
Bo Lee <boleesr@gmail.com>
2018-08-20 15:02:05 -0400

    made getall for grades controller and added grades to server.js

--- app/controllers/GradesController.js
+++ app/controllers/GradesController.js
@@ -1,5 +1,13 @@
 const gradesModel = require('../models/GradesModels');
 
 module.exports = {
-    
-}
+
+  getAll(req, res, next) {
+    gradesModel.getAll()
+      .then((grades) => {
+        res.locals.grades = grades;
+      })
+      .catch(next);
+  },
+
+};
