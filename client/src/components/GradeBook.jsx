6a490f4fe75824ec39b1d0b1e9fc9e1a85a2a664
elizabith.costey <costey.ea@gmail.com>
2018-08-21 16:33:42 -0400

    Added render to gradebook page

--- client/src/components/GradeBook.jsx
+++ client/src/components/GradeBook.jsx
@@ -2,40 +2,42 @@ import React from 'react';
 
 
 function gradeList(props) {
-    return (
-        <div>
-            <h1>Fetch</h1>
-            <h2>Gradebook</h2>
+    render(){
+        return (
             <div>
-                {
-                    <table style="width:100%">
-                        <tr>
-                            <th>Name</th>
-                            <th>Potty Training</th>
-                            <th>Leash Training</th>
-                            <th>Stay</th>
-                            <th>Sit</th>
-                            <th>Quiet</th>
-                            <th>Leave It</th>
-                        </tr>
-                        props.grades.map(grades => (
-                        <div key={this.props.gradebook.dogs_id}>
+                <h1>Fetch</h1>
+                <h2>Gradebook</h2>
+                <div>
+                    {
+                        <table style="width:100%">
                             <tr>
-                                <td>{this.props.dogs.name}</td>
-                                <td>{this.props.gradebook.leash_training}</td>
-                                <td>{this.props.gradebook.potty_training}</td>
-                                <td>{this.props.gradebook.stay}</td>
-                                <td>{this.props.gradebook.sit}</td>
-                                <td>{this.props.gradebook.quiet}</td>
-                                <td>{this.props.gradebook.leave_it}</td>
+                                <th>Name</th>
+                                <th>Potty Training</th>
+                                <th>Leash Training</th>
+                                <th>Stay</th>
+                                <th>Sit</th>
+                                <th>Quiet</th>
+                                <th>Leave It</th>
                             </tr>
-                            </div> 
-                        ))
+                            props.grades.map(grades => (
+                        <div key={this.props.gradebook.dogs_id}>
+                                <tr>
+                                    <td>{this.props.dogs.name}</td>
+                                    <td>{this.props.gradebook.leash_training}</td>
+                                    <td>{this.props.gradebook.potty_training}</td>
+                                    <td>{this.props.gradebook.stay}</td>
+                                    <td>{this.props.gradebook.sit}</td>
+                                    <td>{this.props.gradebook.quiet}</td>
+                                    <td>{this.props.gradebook.leave_it}</td>
+                                </tr>
+                            </div>
+                            ))
                     </table>
-                }
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 
