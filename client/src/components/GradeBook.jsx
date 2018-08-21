1c63326840aca2aaa66ec18f39e39d47482d6c96
elizabith.costey <costey.ea@gmail.com>
2018-08-21 14:13:58 -0400

    map(gradebook) function complete. Need to make a couple more edits and test

--- client/src/components/GradeBook.jsx
+++ client/src/components/GradeBook.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
-function(){
-    const gradesList = props.gradebook.map((gradebook))
+function gradeList(props) {
+    return (
+        <div>
+            <h1>Fetch</h1>
+            <h2>Gradebook</h2>
+            <div>
+                {
+                    <table style="width:100%">
+                        <tr>
+                            <th>Pup's Name</th>
+                            <th>Potty Training</th>
+                            <th>Leash Training</th>
+                            <th>Stay</th>
+                            <th>Sit</th>
+                            <th>Quiet</th>
+                            <th>Leave It</th>
+                        </tr>
+                        props.grades.map(grades => (
+                        <div key={gradebook.dogs_id}>
+                            <tr>
+                                <td>{dogs.name}</td>
+                                <td>{gradebook.potty_training}</td>
+                                <td>{gradebook.leash_training}</td>
+                                <td>{gradebook.stay}</td>
+                                <td>{gradebook.sit}</td>
+                                <td>{gradebook.quiet}</td>
+                                <td>{gradebook.leave_it}</td>
+                            </tr>
+                            </div> 
+                        ))
+                    </table>
+                }
+            </div>
+        </div>
+    )
 }
 
 
